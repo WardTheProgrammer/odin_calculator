82fd5eec9844bc90d1f4b472191feb73df8d3e88
WardTheProgrammer <nijhuis.ward@gmail.com>
2024-02-11 13:50:57 +0100

    Add the basic operations to the new JS file
    Your calculator is going to contain functions for all of the basic math operators you typically find on calculators, so start by creating functions for the following items and testing them in your browser’s console.
    add,
    subtract,
    multiply,
    divide

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = (num1, num2) => {
+    return Number(num1) + Number(num2);
+}
+
+const subtract = (num1, num2) => {
+    return Number(num1) - Number(num2);
+}
+
+const multiple = (num1, num2) => {
+    return Number(num1) * Number(num2);
+}
+
+const divide = (num1, num2) => {
+    return Number(num1) / Number(num2);
+}
